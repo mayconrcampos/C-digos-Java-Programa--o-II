17699d69b5d5e07ba5fd612d0f6331d987fd9a6e
mayconrcampos <maycon.campos@gmail.com>
2021-09-10 22:36:45 -0300

    Comentário longo de explicação

--- Atividade01EquacaoDeSegundoGrau/Main.java
+++ Atividade01EquacaoDeSegundoGrau/Main.java
@@ -1,6 +1,26 @@
 package Atividade01EquacaoDeSegundoGrau;
 
 public class Main {
+
+    /**
+     * Atividade 01 - Explicação da Solução.
+     * 
+     *  - Foram criadas 3 classes principais.
+     *      - Valores - Possui três atributos: a, b e c. Possui método construtor, getters e setters.
+     *      - Equação - Possui três métodos estáticos: EquacaoDeSegundoGrau que recebe o objeto valores como parâmetro.
+     *      - Este primeiro método chama o método CalculaDelta passando o objeto valores como parâmetro. O valor é atribuido a        variável delta.
+     *      - Por último, é chamado o método CalculaRaiz que recebe o delta mais o objeto valores.
+     * 
+     *      - A validação de valores digitados acontece na terceira Classe chamada Read.
+     *      - Dispensando o uso da classe Scanner, ali em Read são utilizadas as classes BufferedReader e                     *        InputStreamReader para inserção de dados via teclado as quais fazem perfeito tratamento de buffer e lixo de  *        memória oriundos da entrada de dados.
+     *      - São utilizados dois métodos... sendo o primeiro readString que faz a leitura do teclado e o segundo método *       readDouble que captura uma string e tenta transformar o dado inserido em Double dentro do bloco Try:
+     *      - Se caso o dado inserido pelo usuário não for um Double válido, ele entra no Catch e retorna o valor 0.0 que é *        utilizado dentro do método main, onde se o usuário digitar 0 ou algum caracter que não seja um Double válido, *        ele entra no laço while para pedir que o usuário digite novamente.
+     *      - O programa só irá aceitar valores numéricos que sejam diferentes de zero, positivos ou negativos. 
+     *      - Assim que o valor for aceito, será transformado em Double.
+     * 
+     * 
+     *      - O programa roda dentro de um laço Do While para que o usuário possa fazer vários cálculos, e após cada um deles *        o programa pergunta se deseja continuar. Ao digitar s ou S ele continua. Se digitar qualquer outro valor ele *        encerra a execução.
+     */
     
     public static void main(String[] args) {
         
